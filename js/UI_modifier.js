f08cbf9c79ed5fce9f75eb71bc33bf7ba9058c40
Amittras <pal.amittras@gmail.com>
2020-11-29 18:24:14 +0530

    Update: Color changing navigation-bar

--- js/UI_modifier.js
+++ js/UI_modifier.js
@@ -1,5 +1,5 @@
-var sideNav;
-
+// marks the active button - needs a lot of tweaking if multiple pages are involved - 
+// this would work with single page only.
 function activate(btn) {
     var target = document.querySelector(`#${btn}`);
     var navBtns = document.querySelectorAll('.nav-wrapper a.btn-flat');
@@ -12,8 +12,37 @@ function activate(btn) {
     });
 }
 
+// function to initialize the sidenav for mobile devices
+var sideNav;
 document.addEventListener('DOMContentLoaded', function () {
-    // this.sideNav = document.querySelectorAll('.sidenav');
     var elems = document.querySelectorAll('.sidenav');
     this.sideNav = M.Sidenav.init(elems, {});
 });
+
+
+// ================NavBar Manipulator Block================
+const callback = (entries, observer) => {
+    const entry = entries[0];
+    const { intersectionRatio, isIntersecting } = entry;
+    // console.log(intersectionRatio, isIntersecting);
+    // toggle class depending on if content div intersects with viewport
+    const navBar = document.querySelector('.nav-wrapper');
+    navBar.classList.toggle('solid-nav', !entry.isIntersecting);
+}
+
+// options lets you control the circumstances under which the observer's callback is invoked
+const options = {
+    // no root provided - by default browser viewport used to check visibility of target
+    // Uncomment below if you want the nav bar to change background color as soon as content div is scrolled
+    rootMargin: "-60px 0px 0px 0px",
+    // only detect if visibility of target element is 100% or not
+    threshold: [1]
+};
+
+// create intersection observer
+const io = new IntersectionObserver(callback, options);
+
+// observe content div 
+const target = document.querySelector('#top-separator');
+io.observe(target);
+// ================NavBar Manipulator Block End================
